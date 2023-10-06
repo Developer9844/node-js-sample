bef560b6e90729a8f25f4fb586f7e83a4f65ef7c
Developer9844 <d3692094@gmail.com>
2023-10-06 11:49:31 +0530

    updated app.zip file

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
